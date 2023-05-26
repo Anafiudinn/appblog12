b5d2fc0e8bc65ca9530c2c7ecc665c8ad0bef625
ParshaNugraha <pramudyashaka27@gmail.com>
2023-05-26 20:40:22 +0700

    lanjutan halaman crud artikel-part2

--- application/views/backend/Article/list.blade.php
+++ application/views/backend/Article/list.blade.php
@@ -1,8 +1,12 @@
 @extends('template.backend.theme')
 @section('content')
 <br>
-<a href="{{ site_url('backend/article/form') }}">Tambah Article</a>
+<a class="btn btn-info" href="{{ site_url('backend/article/form') }}" role="button">Tambah Article</a>
 <br>
+@if(isset($flashdata['notif']))
+<br>
+<div class="alert alert-warning">{{ $flashdata['notif'] }}</div>
+@endif
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
       <div class="table-responsive">
         <table class="table table-striped table-sm">
@@ -13,6 +17,7 @@
               <th scope="col">Article</th>
               <th scope="col">Created At</th>
               <th scope="col">Updated At</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
@@ -23,6 +28,10 @@
               <td>{{ $post->article }}</td>
               <td>{{ $post->created_at }}</td>
               <td>{{ $post->updated_at }}</td>
+              <td>
+                <a class="btn btn-success btn-sm" href="{{ site_url('backend/article/edit/' . $post->id) }}" role="button"><i class="bi bi-pencil-fill"></i></i></a>
+                <a class="btn btn-danger btn-sm" href="{{ site_url('backend/article/hapus/' . $post->id) }}" role="button"><i class="bi bi-trash-fill"></i></a>
+              </td>
             </tr>
             @endforeach
           </tbody>
